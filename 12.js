6a7f87a998e207c9e07e1a35e4ebfe12ff8cf467
zz <146847276+zyh2005ff@users.noreply.github.com>
2024-11-12 17:51:35 +0800

    Update 12.js

--- 12.js
+++ 12.js
@@ -1,1 +1,19 @@
 
+   <script type="text/javascript">
+    function check(){
+        var flag = true;
+        var pass = document.getElementById("pass").value;
+        for(var i = 0;i<pass.length;i++)
+    {
+        if((pass.charAt(i)<"0")||(pass.charAt(i)>"9")){
+            flag = false;
+            break;
+        }
+    }
+    if(flag){
+        alert("密码正确");
+        window.location.href="1-1.html";}
+        else
+        alert("输入不合法！请重新输入");
+    }
+</script>
